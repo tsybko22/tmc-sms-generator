ec3f11a4519b71550809480674ccf408e424fe53
Ivan Tsybko <tsybko22@gmail.com>
2024-04-07 17:01:21 +0300

    change scheme color & fix Header styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import Header from './components/Header';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className='min-h-screen'>
+      <Header />
+      <main></main>
+    </div>
+  );
 };
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,10 +2,10 @@ import messageIcon from '@icons/message.png';
 
 const Header = () => {
   return (
-    <header className='flex min-h-24 items-center justify-center border-b border-neutral-200'>
+    <header className='flex min-h-24 items-center justify-center border-b border-gray-200 bg-white'>
       <div className='flex items-center gap-2'>
-        <h1 className='scroll-m-20 text-3xl font-extrabold tracking-tight lg:text-4xl'>
-          <span className='text-red-600'>ТМЦ</span> SMS Конструктор
+        <h1 className='scroll-m-20 text-3xl font-normal tracking-tight lg:text-4xl'>
+          <span className='font-bold text-red-600'>ТМЦ</span> SMS Конструктор
         </h1>
         <img
           className='h-9 w-9'
