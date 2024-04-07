393ec0e2d2ea1b8c1a0e455dd59418de1765b70e
Ivan Tsybko <tsybko22@gmail.com>
2024-04-07 23:27:05 +0300

    add Separator component

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,5 +19,6 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/prop-types': 0,
   },
 };
