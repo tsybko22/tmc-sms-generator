421d8daaefe90dca41b395493621ce30d0b1cdbe
Ivan Tsybko <tsybko22@gmail.com>
2024-04-07 22:47:38 +0300

    rename files

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import Header from './components/Header';
+import Header from './components/header';
+import Editor from './components/sms-editor';
+import Preview from './components/sms-preview';
 
 const App = () => {
   return (
     <div className='min-h-screen'>
       <Header />
-      <main></main>
+      <main className='grid grid-cols-1 gap-4 lg:grid-cols-2 lg:gap-0'>
+        <Editor />
+        <Preview />
+      </main>
     </div>
   );
 };
