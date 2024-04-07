45265258b5ebaebc848b0d0290540d337e5208fa
Ivan Tsybko <tsybko22@gmail.com>
2024-04-07 22:51:41 +0300

    fix header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import messageIcon from '@icons/message.png';
 
 const Header = () => {
   return (
-    <header className='flex min-h-24 items-center justify-center border-b border-gray-200 bg-white'>
+    <header className='flex min-h-24 items-center justify-center border-b border-neutral-200 bg-white'>
       <div className='flex items-center gap-2'>
-        <h1 className='scroll-m-20 text-3xl font-normal tracking-tight lg:text-4xl'>
+        <h1 className='text-3xl font-normal tracking-tight lg:text-4xl'>
           <span className='font-bold text-red-600'>ТМЦ</span> SMS Конструктор
         </h1>
         <img
