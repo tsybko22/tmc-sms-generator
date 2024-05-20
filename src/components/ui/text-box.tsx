b04e95c8558c4bda8011a18cd7a29734dbad8463
Ivan Tsybko <tsybko22@gmail.com>
2024-05-20 20:18:38 +0300

    fix dark theme colors

--- src/components/ui/text-box.tsx
+++ src/components/ui/text-box.tsx
@@ -10,7 +10,7 @@ const TextBox = ({ className, children }: TextBoxProps) => {
   return (
     <pre
       className={cn(
-        'min-h-[80px] w-full cursor-text whitespace-pre-wrap break-words rounded-md border border-neutral-300 bg-background px-3 py-2 font-inter text-sm dark:border-neutral-700 lg:text-lg',
+        'min-h-[80px] w-full cursor-text whitespace-pre-wrap break-words rounded-md border border-neutral-300 bg-background px-3 py-2 font-inter text-sm dark:border-neutral-700/70 lg:text-lg',
         className
       )}
     >
